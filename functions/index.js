fe8135a17c4d9fdb6aebdfd87d1f81e4ef17dd3d
Mathias Neerup <manee12@student.sdu.dk>
2017-07-05 20:13:42 +0200

    New users are created in /likes/ and in /scores/ (only for likes of page so far...)

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,41 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  //console.log(request.url);
-  //console.log(Object.keys(request.params));
-  console.log(Object.keys(request.query));
-  response.send(request.query['hub.challenge']);
-});
+
+exports.FbLikeHandler = functions.database.ref('/likes/{user_id}').onWrite(event => {
+
+//	console.log("Event type: " + event.eventType);
+//	if(event.eventType == "providers/google.firebase.database/eventTypes/ref.write"){
+//
+//	} else {
+//		console.log("Unhandled event: " + event.eventType);
+//		return;
+//	}
+
+//      console.log("OnWrite() called..");
+      // Only edit data when it is first created.
+//	console.log("before event score: " + event.data.previous.val() + " exists: " + event.data.previous.exists());
+      if (event.data.previous.exists()) {
+	console.log("User has already liked something... Not updating score..");
+        return;
+      }
+
+	console.log("userid: " + event.params.user_id);
+	var scoreRef = admin.database().ref('/scores/' + event.params.user_id);
+	scoreRef.transaction(function(score) {
+	    if (score) {
+  	        console.log("Score: " + score);
+		score=score+10;
+	    } else {
+		console.log("Score not in db yet...");
+		score=10;
+	    }
+	    return score;
+	  });
+
+    });
