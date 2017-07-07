e8c1c6033cc7a53800c63756ed7baf3120b2e850
Rasmus Haugaard <rasmushaugaard@Rasmuss-MacBook-Pro.local>
2017-07-07 14:50:48 +0200

    postlikehandler, commenthandler, pagelikehandler

--- functions/index.js
+++ functions/index.js
@@ -1,41 +1,56 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+const pageLikePoints = 10;
+const postLikePoints = 5;
+const postCommentPoints = 5;
+
 admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+exports.FbPageLikeHandler = functions.database.ref('/pageLikes/{user_id}').onWrite(event => {
+  if (event.data.previous.exists()) {
+    return;
+  }
+  const user_id = event.params.user_id;
+  var scoreRef = admin.database().ref('/scores/' + user_id);
+  scoreRef.transaction(function(score) {
+    if (score) {
+      score=score+pageLikePoints;
+    } else {
+      score=pageLikePoints;
+    }
+    return score;
+  });
+});
 
-exports.FbLikeHandler = functions.database.ref('/likes/{user_id}').onWrite(event => {
+exports.FbPostLikeHandler = functions.database.ref('/postLikes/{post_id}/{user_id}').onWrite(event => {
+  if (event.data.previous.exists()) {
+    return;
+  }
+  const user_id = event.params.user_id;
+  var scoreRef = admin.database().ref('/scores/' + user_id);
+  scoreRef.transaction(function(score) {
+    if (score) {
+      score=score+postLikePoints;
+    } else {
+      score=postLikePoints;
+    }
+    return score;
+  });
+});
 
-//	console.log("Event type: " + event.eventType);
-//	if(event.eventType == "providers/google.firebase.database/eventTypes/ref.write"){
-//
-//	} else {
-//		console.log("Unhandled event: " + event.eventType);
-//		return;
-//	}
-
-//      console.log("OnWrite() called..");
-      // Only edit data when it is first created.
-//	console.log("before event score: " + event.data.previous.val() + " exists: " + event.data.previous.exists());
-      if (event.data.previous.exists()) {
-	console.log("User has already liked something... Not updating score..");
-        return;
-      }
-
-	console.log("userid: " + event.params.user_id);
-	var scoreRef = admin.database().ref('/scores/' + event.params.user_id);
-	scoreRef.transaction(function(score) {
-	    if (score) {
-  	        console.log("Score: " + score);
-		score=score+10;
-	    } else {
-		console.log("Score not in db yet...");
-		score=10;
-	    }
-	    return score;
-	  });
-
-    });
+exports.FbPostCommentHandler = functions.database.ref('/postComment/{post_id}/{user_id}').onWrite(event => {
+  if (event.data.previous.exists()) {
+    return;
+  }
+  const user_id = event.params.user_id;
+  var scoreRef = admin.database().ref('/scores/' + user_id);
+  scoreRef.transaction(function(score) {
+    if (score) {
+      score=score+postLikePoints;
+    } else {
+      score=postLikePoints;
+    }
+    return score;
+  });
+});
