2e710020cbbc8dcddd572cb834bd08f874814505
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2017-09-12 20:13:20 +0200

    added durations for duration highscore and a rebuild firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const _ = require('lodash');
 
 const pageLikePoints = 10;
 const postLikePoints = 5;
@@ -56,6 +57,35 @@ exports.FbPostCommentHandler = functions.database.ref('/postComment/{post_id}/{u
 });
 */
 
+function handleCorrectUserPattern(user, duration){
+  return admin.database().ref()
+    .child('durations').child(user)
+    .transaction(previousDuration => {
+      return previousDuration ? Math.min(previousDuration, duration) : duration;
+    });
+}
+
+exports.adminActionHandler = functions.database.ref('adminActions/{action}').onCreate(event => {
+  const action = event.params.action;
+  const entry = event.data.val();
+  if (action === 'rebuildDurations'){
+    const ref = admin.database().ref();
+    return ref.child('durations').remove()
+      .then(() => ref.child('logs').once('value'))
+      .then(snap => Promise.all(
+        _.values(snap.val())
+          .filter(entry => entry.type === 'correct userpattern')
+          .map(entry => handleCorrectUserPattern(entry.user, entry.duration))
+      ));
+  }
+});
+
+exports.logEventHandler = functions.database.ref('logs/{logId}').onCreate(event => {
+  const entry = event.data.val();
+  if (entry.type == 'correct userpattern')
+    handleCorrectUserPattern(entry.user, entry.duration);
+});
+
 exports.logUserCreation = functions.auth.user().onCreate(function(event) {
   const d = event.data;
   const fb_data = d.providerData.find(pd => pd.providerId === 'facebook.com');
@@ -63,6 +93,7 @@ exports.logUserCreation = functions.auth.user().onCreate(function(event) {
     displayName: d.displayName,
     email: d.email || false,
     photoURL: d.photoURL || false,
+    fbUID: fb_data.uid,
   });
   admin.database().ref('fire2face').child(d.uid).set(fb_data.uid);
   admin.database().ref('face2fire').child(fb_data.uid).set(d.uid);
